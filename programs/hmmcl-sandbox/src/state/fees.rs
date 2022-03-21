941ac36e5a9918de7851487cfe3a1e4231af3804
Lamine Gaye <laminegaye@me.com>
2022-03-21 16:08:26 +0000

    implement Default (scale) for Fee

--- programs/hmmcl-sandbox/src/state/fees.rs
+++ programs/hmmcl-sandbox/src/state/fees.rs
@@ -1,6 +1,8 @@
 use anchor_lang::prelude::*;
 
-#[derive(Debug, Clone, Copy, Default, AnchorSerialize, AnchorDeserialize)]
+pub const FEE_SCALE: u8 = 12;
+
+#[derive(Debug, Clone, Copy, AnchorSerialize, AnchorDeserialize)]
 pub struct Fee {
     /// generic struct for fee handling. fees cannot be negative
     pub fee_scale: u8,
@@ -8,4 +10,16 @@ pub struct Fee {
     pub f_y: u128, // pure transaction fee for token y
     pub h_x: u128, // hmm adjustment fee for token x
     pub h_y: u128, // hmm adjustment fee for token y
+}
+
+impl Default for Fee {
+    fn default() -> Self {
+        Self {
+            fee_scale: FEE_SCALE,
+            f_x: 0,
+            f_y: 0,
+            h_x: 0,
+            h_y: 0,
+        }
+    }
 }
